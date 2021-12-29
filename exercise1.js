3f942f7aa89896945b42ce730c21ead029b432c8
VakhnenkoIlya <mazerfaker@list.ru>
2021-12-29 18:22:46 +0300

    Lesson3

---
+++ exercise1.js
@@ -0,0 +1,12 @@
+"use strict"
+//Задание 1
+for (let i=0; i<11; i++){
+    if(i==0){
+        console.log((i)+" - это ноль");    
+    }
+    if(i!=0 && i%2==0){
+        console.log((i)+" - четное число")
+    }
+    if (i!=0 && i%2==1)
+    console.log((i)+" - нечетное число")
+}
---
+++ exercise6.js
@@ -0,0 +1,5 @@
+"use strict"
+for (let index = "X"; index.length <= 20; index +="X") {
+    
+    console.log (index)
+}
